8429cec4223b2ec5c9af49e8e1766e13ac32322b
Ian Bruns <ian@rbruns.com>
2021-01-14 13:14:09 -0700

    heroku fix

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
+const pg = require('pg');
 
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL
 });
+
+pg.defaults.ssl = process.env.NODE_ENV === 'production';
 
 app.set('db', db);
 
